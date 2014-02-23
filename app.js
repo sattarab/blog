d99f7244fdb65e61826709bea0d6b06d7af88bd6
sattarab <abdullah.sattar@utoronto.ca>
2014-02-22 18:40:09 -0800

    cleaned up initial code

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.configure(function(){
 	app.set('port', 8000);
 	app.set('views', path.join(__dirname, '/client/views'));
 	app.set('view engine', 'ejs');
-	app.use(express.favicon(__dirname + '/client/images/favicon.ico'));
+	app.use(express.favicon());
 	app.use(express.logger('dev'));
 	app.use(express.json());
 	app.use(express.urlencoded());
--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -27,55 +27,10 @@ angular.module('app.controllers', [])
 	}
 })
 .controller('SignupCtrl', function($scope, $rootScope, $http, $location, $anchorScroll, $modal) {
-	$scope.user = {};
-	$scope.errorMessage = false;
-	$scope.isCollapsed = false;
 	
-	$scope.open = function () {
-		var modalInstance = $modal.open({
-			templateUrl: '/views/partials/login.html',
-			controller: 'LoginCtrl'
-		});
-	};
-
-	$scope.signup = function () {
-		$http.post('/api/register', {
-			username: $scope.user.username,
-			password: $scope.user.password
-		})
-		.success(function (user) {
-			$http.post('/api/login', {
-				username: $scope.user.username,
-				password: $scope.user.password
-			})
-			.success(function (user){
-				$location.url('/home');
-			})
-			.error(function (err){
-				console.log(err);
-			})
-		})
-		.error(function (err) {
-			$scope.message = 'username already exists';
-			$scope.errorMessage = true;
-			$location.url('/');
-		});
-	};
 })
 .controller('HomeCtrl', function($scope, $http) {
-	$scope.home = function () {
-		$http.get('/api/home')
-		.success(function (data, status, headers, config) {
-			$scope.data = data;
-			console.log('The data is ', $scope.data);
-			console.log('The status is ', status);
-			console.log('The headers are ', headers);
-		})
-		.error(function (err) {
-			console.log(err);
-			return;
-		})
-	};
+	
 })
 .controller('ForgotPasswordCtrl', function($scope, $http, $location) {
 	$scope.user = {};
